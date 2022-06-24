d1eb8b46f9348eca84a53f5b5df3c0c7ccd0aa6d
eryk <xuqi86@gmail.com>
2022-06-24 11:18:30 +0800

    update: leetcode-26. Remove Duplicates from Sorted Array

--- array/remove_duplicates_from_sorted_array.cpp
+++ array/remove_duplicates_from_sorted_array.cpp
@@ -35,14 +35,14 @@
 using namespace std;
 
 /**
- * 时间复杂度 : O(1)
+ * 时间复杂度 : O(n)
  * 空间复杂度 : O(1)
  * 解题思路：
  *      使用pos表示未重复数据最新的位置，然后从第一个位置开始，跟前一个数字比较是否相等，不相等就替换掉pos，然后pos+1
  * @param nums
  * @return
  */
-int removeDuplicates(vector<int> &nums) {
+int removeDuplicates1(vector<int> &nums) {
     if (nums.empty()) return 0;
     int pos = 1;
     for (int i = 1; i < nums.size(); i++) {
@@ -50,6 +50,43 @@ int removeDuplicates(vector<int> &nums) {
     }
     return pos;
 }
+
+/**
+ * 时间复杂度 : O(n)
+ * 空间复杂度 : O(1)
+ * 解题思路：
+ *      使用STL, distance用于计算两个迭代器的范围内包含元素的个数,
+ *      unique是去除容器或者数组中相邻元素重复出现的元素，去除是将重复元素放到容器的末尾，返回值是去重后的尾指针
+ *      unique针对的是相邻元素，所以对于顺序顺序错乱的数组成员，或者容器成员，需要先进行排序，可以调用std::sort()函数
+ * @param nums
+ * @return
+ */
+int removeDuplicates2(vector<int> &nums){
+    return distance(nums.begin(), unique(nums.begin(), nums.end()));
+}
+
+/**
+ * 时间复杂度 : O(n)
+ * 空间复杂度 : O(1)
+ * 解题思路：
+ *      使用STL, upper_bound返回一个迭代器，指向范围 [first,last) 中比较大于 val 的第一个元素。
+ * @param nums
+ * @return
+ */
+template<typename Init, typename OutIt>
+OutIt removeDuplicates(Init first, Init last, OutIt output){
+    while(first != last){
+        *output++ = *first;
+        first = upper_bound(first, last, *first);
+    }
+    return output;
+}
+
+int removeDuplicates(vector<int> &nums){
+    return distance(nums.begin(), removeDuplicates(nums.begin(), nums.end(), nums.begin()));
+}
+
+
 
 
 int main() {
